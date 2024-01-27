1d2d61dd54d7036432b952655af81e6549fbe19d
fydar <fydar.dev@gmail.com>
2024-01-27 18:42:32 +0000

    🚧 Blocking out the scene and introduction flow

--- src/GlobalGameJam2024Unity/Assets/Scripts/GameController.cs
+++ src/GlobalGameJam2024Unity/Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using GlobalGameJam2024.Simulation.Commands;
 using GlobalGameJam2024.Simulation.Services.Network;
 using System;
@@ -10,11 +11,46 @@ namespace GlobalGameJam2024
 	{
 		[SerializeField] private string url;
 		[SerializeField] private CanvasGroup blackFade;
+		[SerializeField] private CinemachineVirtualCamera cameraCamp;
+		[SerializeField] private CinemachineVirtualCamera cameraPanAcross;
+		[SerializeField] private CinemachineVirtualCamera cameraPullBackStart;
+		[SerializeField] private CinemachineVirtualCamera cameraPullBackMiddle;
+		[SerializeField] private CinemachineVirtualCamera cameraPullBackEnd;
+		[SerializeField] private CinemachineVirtualCamera gameplayCamera;
+
 
 		private ClientService clientService;
 
+		private IEnumerator FadeToBlack(float duration = 1.0f)
+		{
+			foreach (float time in new TimedLoop(duration))
+			{
+				blackFade.alpha = time;
+				yield return null;
+			}
+		}
+
+		private IEnumerator FadeFromBlack(float duration = 1.0f)
+		{
+			foreach (float time in new TimedLoop(duration))
+			{
+				blackFade.alpha = 1.0f - time;
+				yield return null;
+			}
+		}
+
 		public IEnumerator Start()
 		{
+			blackFade.alpha = 1.0f;
+
+			cameraCamp.gameObject.SetActive(true);
+			cameraPanAcross.gameObject.SetActive(false);
+			cameraPullBackStart.gameObject.SetActive(false);
+			cameraPullBackMiddle.gameObject.SetActive(false);
+			cameraPullBackEnd.gameObject.SetActive(false);
+			gameplayCamera.gameObject.SetActive(false);
+
+			// Connect to server...
 			clientService = new ClientService(url);
 
 			while (!clientService.connectionTask.Task.IsCompletedSuccessfully)
@@ -22,12 +58,55 @@ namespace GlobalGameJam2024
 				yield return null;
 			}
 
-			foreach (float time in new TimedLoop(1.0f))
-			{
-				blackFade.alpha = 1.0f - time;
-			}
+			cameraCamp.gameObject.SetActive(true);
+			yield return StartCoroutine(FadeFromBlack());
 
-			yield return null;
+			// Lobby
+			yield return new WaitForSeconds(3.0f);
+
+			yield return StartCoroutine(FadeToBlack(1.0f));
+
+			// Start introduction cinematic by panning across the troops rearranged.
+			cameraCamp.gameObject.SetActive(false);
+			cameraPanAcross.gameObject.SetActive(true);
+
+			yield return StartCoroutine(FadeFromBlack(1.0f));
+
+			yield return new WaitForSeconds(5.0f);
+
+			yield return StartCoroutine(FadeToBlack(2.0f));
+
+			// Now bring the camera behind the soldiers, focused on them.
+			cameraPanAcross.gameObject.SetActive(false);
+			cameraPullBackStart.gameObject.SetActive(true);
+
+			yield return StartCoroutine(FadeFromBlack(1.0f));
+
+			yield return new WaitForSeconds(4.0f);
+
+
+			// Now focus on McFunkypants as he completes his rise.
+			cameraPullBackStart.gameObject.SetActive(false);
+			cameraPullBackMiddle.gameObject.SetActive(true);
+
+			yield return new WaitForSeconds(4.0f);
+
+			// Now bring the camera behind the soldiers, focused on them, with McFunkypants in the background,
+			// as the soldiers ready themselves for battle.
+			cameraPullBackMiddle.gameObject.SetActive(false);
+			cameraPullBackEnd.gameObject.SetActive(true);
+
+			yield return new WaitForSeconds(4.0f);
+
+			// Now transition to the normal gameplay camera.
+			cameraPullBackEnd.gameObject.SetActive(false);
+			gameplayCamera.gameObject.SetActive(true);
+
+			yield return new WaitForSeconds(2.0f);
+			yield return StartCoroutine(FadeToBlack(2.0f));
+			yield return StartCoroutine(FadeFromBlack(2.0f));
+
+			yield return new WaitForSeconds(5.0f);
 
 			while (true)
 			{
